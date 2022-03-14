dbc3a177ddf3692a3699e188e8acbc312443163e
Grant Pauker <grantpauker@gmail.com>
2022-03-13 17:05:28 -0700

    Fix radix tree

--- project4/include/RadixTree.h
+++ project4/include/RadixTree.h
@@ -21,79 +21,76 @@ public:
 
     void insert(std::string key, const ValueType &value)
     {
-        std::string original_key = key;
         Node *parent = nullptr;
-        Node *found = findLocation(m_head, &key, &parent);
+        Node *found = m_head;
 
-        // root node (1)
-        if (found->key.empty())
+        // handle root node
+        if (found->children[key[0]] == nullptr)
         {
-            found->children[key[0]] = new Node(key, value, true);
+            Node *inserted = new Node(key, value, true);
+            found->children[inserted->key[0]] = inserted;
             return;
         }
-        // replace current node (4)
-        if (found->key == key || key == "")
-        {
-            found->value = value;
-            found->end = true;
-            return;
-        }
-        int diff = firstDifference(found->key, key);
 
-        // insert single node below current (5)
-        if (found->children[key[0]] == nullptr && (diff == 0 || diff > found->key.size())) 
+        while (1)
         {
-            found->children[key[0]] = new Node(key, value, true);
-            return;
-        }
-        if (isChildless(found))
-        {
-            // shift current node down and insert one node (6)
-            if (diff > key.size())
-            { 
-                Node *shifted = new Node(found->key.substr(diff - 1), found->value, true);
-                found->key = key;
-                found->value = value;
-                found->end = true;
-                found->children[shifted->key[0]] = shifted;
-                return;
-            }
-            // shift current node down and insert two nodes (2)
-            else
+            parent = found;
+            found = found->children[key[0]];
+            // insert the new pair directly below found node if we can
+            if (found == nullptr)
             {
-                std::string prefix = found->key.substr(0, diff);
-                Node *shifted = new Node(found->key.substr(diff), found->value, true);
-                Node *inserted = new Node(key.substr(diff), value, true);
-                found->key = prefix;
-                found->end = false;
-                found->children[shifted->key[0]] = shifted;
-                found->children[inserted->key[0]] = inserted;
+                Node *inserted = new Node(key, value, true);
+                parent->children[inserted->key[0]] = inserted;
                 return;
             }
-        }
-        else
-        {
-            // shift current node up and insert one node (7)
-            if (diff > key.size())
-            { 
+            // given key is already in tree
+            if(key == found->key){
+                found->end = true;
+                found->value = value;
+                return;
+            }
+            //  found key is at beggining of current key; cut it and continue
+            if (key.find(found->key) == 0)
+            {
+                key = key.substr(found->key.size());
+            }
+            //  current key is at beginning of found key; cut the found key and insert the new key
+            else if (found->key.find(key) == 0)
+            {
                 Node *inserted = new Node(key, value, true);
-                found->key = found->key.substr(diff - 1);
-                found->end = false; 
+                found->key = found->key.substr(key.size());
                 inserted->children[found->key[0]] = found;
                 parent->children[inserted->key[0]] = inserted;
+                return;
             }
-            // shift current node up and insert two nodes (4)
             else
             {
-                std::string prefix = found->key.substr(0, diff);
-                Node *shifted = new Node(prefix, false);
-                Node *inserted = new Node(key.substr(diff), value, true);
-                found->key = found->key.substr(diff);
-                found->end = false;
-                shifted->children[found->key[0]] = found;
-                shifted->children[inserted->key[0]] = inserted;
-                parent->children[shifted->key[0]] = shifted;
-                return;
+                int diff = firstDifference(key, found->key);
+                // if the node has children shift everything down
+                if (isChildless(found))
+                {
+                    std::string prefix = found->key.substr(0, diff);
+                    Node *shifted = new Node(found->key.substr(diff), found->value, true);
+                    Node *inserted = new Node(key.substr(diff), value, true);
+                    found->key = prefix;
+                    //found->end = false; 
+                    found->children[shifted->key[0]] = shifted;
+                    found->children[inserted->key[0]] = inserted;
+                    return;
+                }
+                // if the node doesn't have children shift everything up
+                else
+                {
+                    std::string prefix = found->key.substr(0, diff);
+                    Node *shifted = new Node(prefix, false);
+                    Node *inserted = new Node(key.substr(diff), value, true);
+                    found->key = found->key.substr(diff);
+                    //found->end = false; 
+                    shifted->children[found->key[0]] = found;
+                    shifted->children[inserted->key[0]] = inserted;
+                    parent->children[shifted->key[0]] = shifted;
+                    return;
+                }
             }
         }
     }
@@ -192,32 +189,6 @@ private:
             }
         }
         return i + 1;
-    }
-
-    Node *findLocation(Node *cur, std::string *key, Node **parent) const
-    {
-        char c = (*key)[0];
-        if (cur->children[c] == nullptr)
-        {
-            return cur;
-        }
-        *parent = cur;
-        cur = cur->children[c];
-
-        if (*key == cur->key && cur->end)
-        {
-            return cur;
-        }
-        if (firstDifference(*key, cur->key) < std::min(key->size(), cur->key.size()))
-        {
-            return cur;
-        }
-        if (key->rfind(cur->key, 0) == 0)
-        {
-            *key = key->substr(cur->key.size());
-            return findLocation(cur, key, parent);
-        }
-        return cur;
     }
 
     Node *search(Node *cur, std::string key) const
